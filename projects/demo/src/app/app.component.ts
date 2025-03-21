c046d941aae48234ba693141dad065018228281a
David Wendland <dwndland@gmail.com>
2025-03-21 21:20:09 +0100

    Add demo tab for the DockPanel

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import { DockPanelComponent } from "../../../chapter-ang-controls/src/lib/dock-panel/dock-panel.component";
 
 @Component({
     selector: 'app-root',
     imports: [
-        MatTabsModule
-    ],
+    MatTabsModule,
+    DockPanelComponent,
+    MatButtonModule
+],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
